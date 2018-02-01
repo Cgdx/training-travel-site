5a81c0c77911666fe9465d4d2e27528319129464
Chris <workstationfr@gmail.com>
2018-02-01 21:45:29 +0100

    Completed desktop and mobile header style

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,5 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
-
-alert("ABC 321");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
